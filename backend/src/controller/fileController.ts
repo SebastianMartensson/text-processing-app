88fd12db703dd15a1cbcec353d6b7ad34efc53d1
SebastianMartensson <sebastian.martensson95@gmail.com>
2024-03-20 21:49:16 +0100

    update, error handling and update index.tsx file

--- backend/src/controller/fileController.ts
+++ backend/src/controller/fileController.ts
@@ -3,14 +3,14 @@ import fs from 'fs';
 
 export async function fileHandler(req: Request, res: Response, next: NextFunction) {
     try {
+        throw new Error("oopsie");
         const fileContent = fs.readFileSync(req.file!.path, 'utf-8');
         const mostUsedWord = findMostUsedWord(fileContent);
         const processedText = fileContent.replace(new RegExp(mostUsedWord, 'g'), `foo${mostUsedWord}bar`);
 
         res.status(200).json({ processedText });
     } catch (error) {
-        console.error('Error processing file:', error);
-        res.status(500).json({ error: 'Internal server error' });
+        next(error);
     }
 }
 
